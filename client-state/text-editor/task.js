cd694407d27e653ecf7e4168097914f85e2676b1
Ibragim <gamemega.ao@gmail.com>
2023-10-31 17:30:26 +0300

    text-editor-2

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,11 +1,18 @@
 "use strict";
 
 const editor = document.getElementById('editor');
+const clearButton = document.getElementById('clearButton');
 editor.addEventListener('input', handleInput);
+clearButton.addEventListener('click', handleClear);
 
 function handleInput() {
     const value = editor.value;
     localStorage.setItem('editorValue', value);
+}
+
+function handleClear() {
+    editor.value = '';
+    localStorage.removeItem('editorValue');
 }
 
 window.addEventListener('load', () => {
